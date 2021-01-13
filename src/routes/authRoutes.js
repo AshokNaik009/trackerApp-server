0cfbab4d800fab1888c4bb940b9da273689a15b3
ashok <ashok.quickdesks@gmail.com>
2021-01-13 11:16:18 +0530

    Error handling completed and created JSON web token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const mongoose =require('mongoose');
-const User =  mongoose.model('User');
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = mongoose.model("User");
 const router = express.Router();
 
-router.post('/signup',(req,res)=>{
-    console.log(req.body)
-    res.send('A post request');
-})
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    //To do: Generate a different Secret Key
+    const token = jwt.sign({ userId: user._id }, "DUMMY_SECRET_KEY");
+    res.send({ token });
+  } catch (error) {
+    return res.status(422).send(error.message);
+  }
+});
 
-module.exports =router;
+module.exports = router;
