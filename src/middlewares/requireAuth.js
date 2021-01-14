f11954eb2b631205d69ee512c4020514fd3187dc
ashok <ashok.quickdesks@gmail.com>
2021-01-14 11:44:26 +0530

    Created Authorization Middleware and implemented password hashing

---
+++ src/middlewares/requireAuth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+module.exports = (req,res,next) =>{
+    const {authorization} = req.headers;
+
+    if(!authorization) {
+        return res.status(401).send({error:'Please login'})
+    }
+
+    const token = authorization.replace('Bearer ','');
+    jwt.verify(token,"DUMMY_SECRET_KEY",async (err,payload) =>{
+        if(err) {
+            return res.status(401).send({error:'Please login'}) 
+        }
+        const {userId} = payload;
+        const user = await User.findById(userId);
+        console.log(user)
+        req.user = user;
+        next();
+    })
+
+}
